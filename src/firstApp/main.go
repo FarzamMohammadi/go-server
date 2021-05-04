820adf32d3706c4559bc47e828a65af9751097ca
FarzamMohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2021-05-03 21:21:30 -0400

    Dockerized The App

--- src/firstApp/main.go
+++ src/firstApp/main.go
@@ -1,10 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
+
+	fmt.Print("App Started")
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("World"))
 	})
 	http.ListenAndServe(":8080", nil)
+
+	fmt.Print("End")
+
 }
