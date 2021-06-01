8aeb74961facae137da5776fba0721db4eec9c60
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2021-05-31 23:48:27 -0400

    First Postgres Connection Attempt

--- src/firstApp/main.go
+++ src/firstApp/main.go
@@ -4,10 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
 )
+
+type Visit struct {
+	gorm.Model
+
+	Time string
+}
 
 func main() {
 
+	//Server
 	fmt.Println("App Started")
 	http.HandleFunc("/hello", handler)
 
@@ -15,9 +27,26 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	//Database
+	e := godotenv.Load() //Load .env file
+	if e != nil {
+		fmt.Print(e)
+	}
+
+	db, err := gorm.Open("postgres", "host=db port=7567 user=postgres dbname=postgres sslmode=disable password=postgres")
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	defer db.Close()
+
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("World"))
 	w.WriteHeader(http.StatusOK)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/hello", AddTime).Methods("POST", "")
 }
